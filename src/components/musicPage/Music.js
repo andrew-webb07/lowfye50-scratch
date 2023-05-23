bdaa0a9b415859215f89930b61278aa22dc852f4
Andrew Webb <webba@ihopu.org>
2023-05-23 16:12:40 -0500

    added pre-save link

--- src/components/musicPage/Music.js
+++ src/components/musicPage/Music.js
@@ -9,6 +9,10 @@ export const Music = () => (
             {/* <h2 className="tourDateHeader">Tour Dates</h2> */}
 
             <div className="firstMusicItem">
+                <a target="_blank" href="https://orcd.co/dryqjrx" className="musicIcon fab preSave">PRE-SAVE</a>
+            </div>
+
+            <div>
                 <a target="_blank" href="https://open.spotify.com/artist/72ZHgiAkpuDJC9P2C2HGZI?si=T9yidNLCQzeM20oDpu-epA" className="musicIcon fab fa-spotify">Spotify</a>
             </div>
 

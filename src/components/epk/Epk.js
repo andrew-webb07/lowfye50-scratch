258923aeef2785a6b6847f8b729a0a8cd3482187
Andrew Webb <webba@ihopu.org>
2024-11-04 13:26:12 -0600

    add these horns

--- src/components/epk/Epk.js
+++ src/components/epk/Epk.js
@@ -2,6 +2,7 @@ import React from "react"
 import lowfye50black from "../images/RedLowfye50.jpg"
 import bandPic from "../images/musicVideoShootGreen.jpg"
 import honey_locust from "../audio/Honey_Locust.mp3"
+import these_horns from "../audio/these_horns.mp3"
 import techRider from "../images/Lowfye50_4_piece_w_Jared.png"
 import { PhotoSection } from "./PhotoSection"
 import { PromoVideo } from "./PromoVideo"
@@ -37,10 +38,11 @@ export const Epk = () => (
                     <iframe className="spotifySingleEpk" src="https://open.spotify.com/embed/track/5Xu0COMu0mCtKzPdtpePaz?utm_source=generator" width="100%" height="380" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
                 </div>
             </div>
+
             <div className="mp3Box solid">
-                <div className="track">Honey Locust</div>
+                <div className="track">These Horns</div>
                 <div className="audioTrack">
-                <audio controls><source src={honey_locust} type="audio/mpeg" /></audio>
+                <audio controls><source src={these_horns} type="audio/mpeg" /></audio>
                 </div>
             </div>
         </div> 
